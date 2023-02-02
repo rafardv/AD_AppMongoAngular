7686471462c77816220196349db13682d4d400ee
rafaelramdeviu <rafael.ramdeviu.paya@cieep.com>
2023-02-02 17:09:14 +0100

    Ionic user page login

--- userIonic/src/app/app.module.ts
+++ userIonic/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import {SwiperModule} from "swiper/angular";
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SwiperModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- userIonic/src/app/pages/login/login.page.ts
+++ userIonic/src/app/pages/login/login.page.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import Swiper from "swiper";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit
+{
 
+  @ViewChild('slidePrincipal', {static: true}) slides!: Swiper;
+  avatars: {img: string; selccionado: boolean}[] =
+    [
+      {
+        img: 'av-1.png',
+        selccionado: true
+      },
+      {
+        img: 'av-2.png',
+        selccionado: true
+      },
+      {
+        img: 'av-3.png',
+        selccionado: true
+      },
+      {
+        img: 'av-4.png',
+        selccionado: true
+      },
+      {
+        img: 'av-5.png',
+        selccionado: true
+      },
+      {
+        img: 'av-6.png',
+        selccionado: true
+      },
+      {
+        img: 'av-7.png',
+        selccionado: true
+      }
+    ];
+
+  avatarSlide = {
+    slidesPerView: 3.4
+  }
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    this.slides.isLocked;
   }
 
+  login(fLogin: NgForm)
+  {
+    console.log(fLogin.valid);
+  }
+
+  registro(fRegistro: NgForm)
+  {
+    console.log(fRegistro.valid);
+  }
+  seleccionarAvatar(avatar: {img: string; seleccionado: boolean})
+  {
+    this.avatars.forEach(av => av.selccionado = false);
+    avatar.seleccionado = true;
+  }
+
+  mostrarRegistro()
+  {
+    this.slides.isLocked;
+    this.slides.slideTo(0);
+    this.slides.isLocked;
+  }
+
+  mostrarLogin()
+  {
+    this.slides.isLocked;
+    this.slides.slideTo(1);
+    this.slides.isLocked;
+  }
 }
